98e9f9a7a134e80f7b64ee051bae60444fb0631f
Frank Nowinski <frankpnowinski@gmail.com>
2017-04-08 02:09:20 -0400

    Changed Gmail password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,15 +83,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'https://profit-app.herokuapp.com' }
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'profit-app.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'https://profit-app.herokuapp.com',
+    domain:               'profit-app.herokuapp.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
